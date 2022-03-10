f3a9a33110266a9df96b49bf4ce4251ae1d2d89c
Andrzej Iwaszkewicz <jedrek.iwaszkiewicz@gmail.com>
2022-03-10 21:16:53 +0100

    Liczby pierwsze

--- cod/LiczbyPierwszeAI/LiczbyPierwszeAI.cpp
+++ cod/LiczbyPierwszeAI/LiczbyPierwszeAI.cpp
@@ -3,5 +3,25 @@ using namespace std;
 
 int main()
 {
-	
+	int n;
+	int lp = 0;
+	int p = 2;
+
+	cout << "Ile liczb pierwszych chcesz wyswietlic: ";
+	cin >> n;
+	cout << "Liczby pierwsze: ";
+
+	while (lp < n)
+	{
+		for (int d = 2; d <= p - 1; d++)
+		{
+			if (p % d == 0)
+				goto K8;
+		}
+		cout << p << ", ";
+		lp++;
+
+	K8:
+		p++;
+	}
 }
