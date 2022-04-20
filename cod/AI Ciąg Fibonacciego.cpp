e8e806b12c0c858ddc2bb4a949305d50ed91c0c6
NEDRI <100300622+NEDRI@users.noreply.github.com>
2022-04-20 20:30:17 +0200

    AI Ciąg Fibonacciego

---
+++ cod/AI Ciąg Fibonacciego.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    int n = 0;
+    cout << "numer liczby Fibonacciego.. ";
+    cin >> n;
+
+    int f1,f0,f;
+    f0 = 0;
+    f1 = 1;
+    for (int i = 0; i <= n; i++)
+    {
+        if (i>1)
+        {
+            f = f0 + f1;
+            f0 = f1;
+            f1 = f;
+        }
+        else
+        {
+            f = i;
+            continue;
+        }
+        cout << f << "\n";
+    }
+
+
+}
